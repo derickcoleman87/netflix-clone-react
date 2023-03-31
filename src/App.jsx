ce87a687c44a1ac40903b5e0b6e0ecf00dc7240d
derickcoleman87 <107764740+derickcoleman87@users.noreply.github.com>
2023-03-31 05:43:06 -0400

    Started adding modal to more info button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header.jsx";
 import Main from "./Main.jsx";
+import Modal from "./Modal.jsx";
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
   return (
     <div className="app">
       <Header />
       <Main />
+      {openModal ? <Modal /> : null}
     </div>
   );
 }
---
+++ src/Modal.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Modal.css";
+
+function Modal() {
+  return <div className="modal">modal</div>;
+}
+
+export default Modal;
